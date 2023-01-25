524d512f8f523866709e357eb64aafe67cb0bf6f
Jory Griffith <jory.griffith@gmail.com>
2023-01-25 10:10:05 -0500

    Looked at the HWI data.

--- HWI_data_merge.R
+++ HWI_data_merge.R
@@ -17,6 +17,30 @@ both <- merge(species, HWI, by.x="scientific_name", by.y="Species.name")
 write.csv(both, "data-raw/species_traits/aves_HWI.csv")
 
 
+### Look at how many bird species we have dispersal for
+dispersal <- read.csv("data-raw/species_traits/dispersal-distance-data_unsearched.csv")
+
+d <- dispersal %>% filter(class=="Aves")
+
+aves.dispersal <- merge(d, species, by.x="scientificName", by.y="scientific_name")
+# there are 8 out of 26 that have dispersal information
+
+# now merge with HWI
+HWI.dispersal <- merge(aves.dispersal, HWI, by.x="scientificName", by.y="Species.name", all.x=T)
+# we have 6 species wtihout HWI or dispersal distance
+
+
+ggplot(HWI.dispersal, mapping=aes(y=DispersalDistance, x=HWI))+
+  geom_point()+
+  geom_smooth()
+
+
+
+
+
+
+
+
 
 
 
