5b0c1090a2e6b8af83108aad1775ac89890e3b2a
Nikki Moore <nicole.moore@mail.mcgill.ca>
2023-02-08 14:57:46 -0500

    adding dataset with dispersal distance estimates at the order/class level

--- R/02_get-dispersal-data.R
+++ R/02_get-dispersal-data.R
@@ -7,6 +7,9 @@ library(traitdataform)
 library(data.table)
 source("R/harmonize.R")
 
+##-----------------------------------##
+##  collate dispersal distance data  ##
+##-----------------------------------##
 ## read in list of final species 
 sp <- read.csv("data-processed/final-species-list.csv")
 
@@ -75,11 +78,12 @@ sam_sub <- sam_unique %>%
          Database = "Straus et al. (unpub)",
          Sex = NA,
          Code = "DispersalDistance",
+         DispersalDistanceKm = DispersalDistance,
          Field = "dispersal_km",
          ObservationTypeGeneral = NA,
          ObservationTypeSpecific = NA) %>%
   select(scientificName, DispersalDistance, Source, Sex, Unit, Code, Field, ObservationTypeSpecific,
-         ObservationTypeGeneral, Database)
+         ObservationTypeGeneral, Database, DispersalDistanceKm)
   
 sam_sub <- left_join(sam_sub, select(sp, -initial_name, -genus, -species))
 
@@ -98,26 +102,187 @@ dd_tofill = left_join(sp, disp_data)
 write.csv(dd_tofill, "data-raw/species_traits/dispersal-distance-data_unsearched.csv", row.names = F)
 
 
-## visualize
-dd = dd_tofill
-dd$DispersalDistance <- as.numeric(as.character(dd$DispersalDistance))
+##--------------------------------------------------------##
+##  estimate dispersal potential for species in our data  ##
+##--------------------------------------------------------##
+## read in new species list 
+taxa <- read.csv("data-processed/taxa.UPDATED.csv")
 
-dd$DispersalDistanceKm = ifelse(dd$Unit == "m", dd$DispersalDistance/1000, dd$DispersalDistance)
+## Assigning species a dispersal distance:
+## - using the dataset of empirical dispersal estimates we collated, we assigned each species a dispersal distance based on averages calculated by class or order
+## - if more than 10 observations of dispersal events were available at the order level, we calculated a mean at the order level. otherwise, we calculated a mean at the class level
 
-dd %>%
-  filter(!is.na(DispersalDistanceKm)) %>%
-  ggplot(aes(x = DispersalDistanceKm, fill = order)) + geom_histogram() + scale_x_log10()
+## summarize types of measurements by order and class:
+order_dd <- dd %>%
+  filter(order %in% sp$order) %>%
+  group_by(order, ObservationTypeSpecific) %>%
+  tally() %>%
+  spread(key = ObservationTypeSpecific, value = n) %>%
+  filter(!is.na(order))
 
-dd %>%
-  filter(!is.na(DispersalDistanceKm)) %>%
-  View 
-  ggplot(aes(y = ObservationTypeGeneral, fill = class)) + geom_bar() 
+class_dd <- dd %>%
+  filter(class %in% sp$class) %>%
+  group_by(class, ObservationTypeSpecific) %>%
+  tally() %>%
+  spread(key = ObservationTypeSpecific, value = n) %>%
+  filter(!is.na(class))
 
-## within movement studies, what kind of data?
-dd %>%
-  filter(!is.na(DispersalDistanceKm)) %>%
-  filter(ObservationTypeGeneral == "movement study") %>%
-  ggplot(aes(y = ObservationTypeSpecific, fill = class)) + geom_bar() 
+## we included movement studies, mark-recapture studies, radiotagging studies, or natal/breeding dispersal studies 
+
+## create column that identifies studies as either mark-recapture, breeding, or movement 
+dd_reclass <- dd %>%
+  mutate(dispersal_estimate_type = ifelse(ObservationTypeSpecific 
+                                          %in% c("mark-release-recapture",
+                                                 "trapping/radio tracking",
+                                                 "mark-release-recapture and/or radio-tracking studies",
+                                                 "mark-recapture",
+                                                 "capture-mark-recapture"),
+                                                 "mark-recapture study",
+                                                 ifelse(ObservationTypeSpecific 
+                                                        %in% c("breeding dispersal",
+                                                               "natal dispersal",
+                                                               "natal dispersal distance"),
+                                                        "breeding/natal dispersal",
+                                                        ifelse(ObservationTypeSpecific 
+                                                               %in% c("radio-tracking",
+                                                                      "individual movement distance",
+                                                                      "homing study"),
+                                                               "movement study",
+                                                               "other"))))
+
+ggplot(dd_reclass, aes(x = log(DispersalDistanceKm), fill = dispersal_estimate_type)) + geom_histogram()
+
+## filter out "other" types of observations 
+dd_reclass <- filter(dd_reclass, dispersal_estimate_type != "other")
+## fix reptile class:
+dd_reclass <- dd_reclass %>%
+  mutate(order = ifelse(class %in% c("Squamata", "Testudines"),
+                        class, 
+                        order)) %>%
+  mutate(class = ifelse(class %in% c("Squamata", "Testudines"),
+                        "Reptilia", 
+                        class)) %>%
+  filter(!family %in% c("Cheloniidae", "Dermochelyidae")) ## get rid of sea turtles
+
+## calculate dataframe of class and order level means 
+## filter out ones with sample size of less than 10
+class_means <- dd_reclass %>%
+  group_by(class) %>%
+  summarise(ClassMeanDD = mean(DispersalDistanceKm),
+            ClassMeanN = length(DispersalDistanceKm),
+            ClassSD = sd(DispersalDistanceKm)) %>%
+  filter(class %in% taxa$class) %>%
+  filter(ClassMeanN >= 10)
+  
+order_means <- dd_reclass %>%
+  group_by(order) %>%
+  summarise(OrderMeanDD = mean(DispersalDistanceKm),
+            OrderMeanN = length(DispersalDistanceKm),
+            OrderSD = sd(DispersalDistanceKm)) %>%
+  filter(order %in% taxa$order) %>%
+  filter(OrderMeanN >= 10) 
+
+## join 
+dd_ourspp <- left_join(taxa, order_means) %>%
+  left_join(., class_means) 
+
+## write out:
+write.csv(dd_ourspp, "data-processed/taxa-list-with-dispersal.csv", row.names = F)
+
+
+## for bats, we split species into long and short dispersers based on information about migration and movement (most were tagging studies) found through a targeted search
+
+
+
+
+
+
+
+
+## data visualization
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class == "Aves") %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class == "Aves") %>%
+  filter(dispersal_estimate_type != "movement study") %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(order) %>%
+  filter(order %in% taxa$order) %>%
+  filter(class == "Aves") %>%
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(order) %>%
+  filter(order %in% taxa$order) %>%
+  filter(class == "Aves") %>%
+  filter(dispersal_estimate_type != "movement study") %>%
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm)) 
+
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class == "Mammalia") %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class == "Mammalia") %>%
+  filter(dispersal_estimate_type != "movement study") %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(order) %>%
+  filter(order %in% taxa$order) %>%
+  filter(order != "Chiroptera") %>%
+  filter(class == "Mammalia") %>%
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(order) %>%
+  filter(order %in% taxa$order) %>%
+  filter(order != "Chiroptera") %>%
+  filter(class == "Mammalia") %>%
+  filter(dispersal_estimate_type != "movement study") %>%
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm)) 
+
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class == "Amphibia") %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class == "Amphibia") %>%
+  filter(dispersal_estimate_type != "movement study") %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(order) %>%
+  filter(order %in% taxa$order) %>%
+  filter(class == "Amphibia") %>%
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(order) %>%
+  filter(order %in% taxa$order) %>%
+  filter(class == "Amphibia") %>%
+  filter(dispersal_estimate_type != "movement study") %>%
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm))
+
+## for reptiles, only have movement studies 
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class %in% c("Squamata", "Testudines")) %>%
+  summarise(mean = mean(DispersalDistanceKm)) 
+dd_reclass %>%
+  group_by(class) %>%
+  filter(class %in% taxa$order) %>% 
+  filter(class %in% c("Squamata", "Testudines")) %>%
+  filter(!family %in% c("Cheloniidae", "Dermochelyidae")) %>% ## get rid of sea turtles
+  summarise(mean = mean(DispersalDistanceKm),
+            n = length(DispersalDistanceKm)) 
 
 
 
